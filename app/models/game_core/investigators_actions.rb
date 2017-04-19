575e79816083e9b5ed348bddb64034acae1ef536
ced <zuger.cedric@gmail.com>
2017-04-19 19:42:42 +0200

    Tests fixed.

--- app/models/game_core/investigators_actions.rb
+++ app/models/game_core/investigators_actions.rb
@@ -20,15 +20,18 @@ module GameCore
               # Events for investigators
               @game_board.ready_for_events_investigators.each do |i|
                 i.ia_play_events( @game_board, @prof )
+                # Break out of the investigators loop
                 break if @game_board.prof_asked_for_fake_cities?
               end
+              # Break out of the while
+              break if @game_board.prof_asked_for_fake_cities?
             else
               raise "Bad aasm_state : #{@game_board.aasm_state}"
           end
         end
 
         # Once all investigators have played, we goes to next turn
-        unless some_investigator_is_ready_to_play?
+        unless @game_board.prof_asked_for_fake_cities? || some_investigator_is_ready_to_play?
           @game_board.next_turn
         end
 
